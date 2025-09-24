481580f185f486772cb80f47262fe7113ca84cb5
justyncannavaro2428250027-create <sasamerisa05@gmail.com>
2025-09-24 20:38:36 +0700

    pertemuan 24 september 2025

---
+++ routes/web.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const web = express.Router()
+
+web.get('/', (req, res) => {
+    res.render('index')
+})
+web.get('/:username', (req,res) => {
+    const username = req.params.username
+    res.render('public-profile', {
+        title : username,
+        username : username,
+        bio : "Lalalalala"
+    })
+})
+
+export default web
